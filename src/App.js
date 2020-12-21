7d5bb312daf1d4e3fa40c24d5f6dd7770d81f734
dongthoigian365 <37582882+dongthoigian365@users.noreply.github.com>
2020-12-21 20:22:14 +0700

    me

--- src/App.js
+++ src/App.js
@@ -1,17 +1,46 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { MyGlobalContext } from './components/context/MyGlobalContext'
+import axios from 'axios'
+import ClipLoader from "react-spinners/ClipLoader"
+import 'antd/dist/antd.css'
 
 import UserList from './components/unit-23/UserList'
 
+
+
 const App = () => {
-  const [gContext] = useState({
+  const [loading, setLoading] = useState(true)
+  const [gContext, setGContext] = useState({
     user: null
   })
+
+  const getCurrentUser = id => {
+    axios.get(`https://jsonplaceholder.typicode.com/users/${id}`)
+    .then(response => {
+      const user = response.data
+      setGContext({
+        user
+      })
+      setLoading(false)
+    })
+  }
+
+  useEffect(() => {
+    getCurrentUser(2)
+  }, [])
 
   return (
     <>
       <MyGlobalContext.Provider value={gContext}>
-        <UserList />
+        {
+          !loading && <UserList />          
+        }
+        <ClipLoader
+          css={{}}
+          size={150}
+          color={"#123abc"}
+          loading={loading}
+        />
       </MyGlobalContext.Provider>
     </>
   )
--- src/components/unit-23/UserList.js
+++ src/components/unit-23/UserList.js
@@ -1,9 +1,97 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
+import axios from 'axios'
+import { Table, Tag, Button } from 'antd'
+import { MyGlobalContext } from './../context/MyGlobalContext'
+import { UserOutlined, EyeOutlined, EditOutlined, DeleteOutlined } from '@ant-design/icons';
+import { Popconfirm, message } from 'antd'
+
 
 const UserList = () => {
+  const [users, setUsers] = useState([])
+  const gContext = useContext(MyGlobalContext)
+
+  const deleteUser = user => {
+    const newUsers = users.filter(u => u.id !== user.id)
+    setUsers(newUsers)
+  }
+
+  const collumns = [
+    {
+      title: 'ID',
+      dataIndex: 'id',
+      key: 'id',
+      fixed: 'left'
+    },
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+      render: (name, row) => {
+        if (row.id === gContext.user.id) {
+          return (
+            <Tag>
+              <UserOutlined /> {' '}
+              {name}
+            </Tag>
+          )
+        } else {
+          return name
+        }
+      }
+    },
+    {
+      title: 'User name',
+      dataIndex: 'username',
+      key: 'username'
+    },
+    {
+      title: 'Email',
+      dataIndex: 'email',
+      key: 'email'
+    },
+    {
+      title: 'Action',
+      key: 'action ',
+      fixed: 'right',
+      render: (cell, row) => {
+        return (
+          <>
+            <Button shape='circle'>
+              <EyeOutlined />
+            </Button>
+            <Button shape='circle'>
+              <EditOutlined />
+            </Button>
+            <Popconfirm
+              title="Ban co muon xoa dong nay"
+              onConfirm={() => deleteUser(row)}
+              onCancel={() => { }}
+              okText="Yes"
+              cancelText="No"
+            >
+              <Button shape='circle' danger>
+                <DeleteOutlined />
+              </Button>
+            </Popconfirm>
+          </>
+        )
+      }
+    },
+
+  ]
+
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/users')
+    .then(response => {
+      setUsers(response.data)
+    })
+  }, [])
+
   return (
     <>
-    
+      <h1>Danh sach user</h1>
+
+      <Table rowKey='id' dataSource={users} columns={collumns} />
     </>
   )
 }
