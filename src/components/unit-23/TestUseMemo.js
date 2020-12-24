83d9b8b9c3ba1ac1de3f098b2cf2b82a5bded79b
dongthoigian365 <37582882+dongthoigian365@users.noreply.github.com>
2020-12-24 19:19:39 +0700

    vd ve useReducer

--- src/components/unit-23/TestUseMemo.js
+++ src/components/unit-23/TestUseMemo.js
@@ -1,25 +1,50 @@
-import React, { useState, useEffect, useMemo } from 'react'
+import React, { useState, useEffect, useMemo, useReducer } from 'react'
+
+const myReducer = (state, action) => {
+  switch(action.type) {
+    case 'ICREMENT': {
+      return {
+        ...state,
+        count: state.count + 1
+      }
+    }
+    case 'DECREMENT': {
+      return {
+        ...state,
+        count: state.count - 1
+      }
+    }
+    default:
+      return state
+  }
+}
 
 const TestUseMemo = () => {
-  const [count, setCount] = useState(1)
-  const [a, setA] = useState(2)
-  const [b , setB] = useState(3)
-  const [name] = useState('Nguyen Van A')
+  // const [count, setCount] = useState(1)
+  // const [a, setA] = useState(2)
+  // const [b , setB] = useState(3)
+  // const [name] = useState('Nguyen Van A')
+  const [state, dispatch] = useReducer(myReducer, {count: 2, name: 'Nguyen Van A'})
 
-  useMemo(() => {
-    return name
-  }, [name])
+  // const c = useMemo(() => {
+  //   // console.log('___useMemo dc goi')
+  //   return a + b
+  // }, [a, b])
 
-  useEffect(() => {
-    console.log('___useEffect')
-  }, [name])
+  // console.log('sum', c)
+  // useEffect(() => {
+  //   console.log('___useEffect')
+  // }, [name])
 
   return (
     <>
       <h1>TestUseMemo</h1>
-      <h1>Count: {count}</h1>
-      <h1>Sum: {a + b}</h1>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <h1>Count: {state.count}</h1>
+      {/* <h1>Sum: {c}</h1> */}
+      {/* <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setA(a + 1)}>Increment A</button> */}
+      <button onClick={() => dispatch({type: 'ICREMENT'})}>Increment Count</button>
+      <button onClick={() => dispatch({type: 'DECREMENT'})}>Decrement Count</button>
     </>
   )
 }
