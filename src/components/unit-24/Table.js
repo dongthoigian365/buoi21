35f7574512b4c19adb3acd81c09dc4380512ae11
dongthoigian365 <37582882+dongthoigian365@users.noreply.github.com>
2020-12-26 20:11:15 +0700

    up tiep buoi 24

--- src/components/unit-24/Table.js
+++ src/components/unit-24/Table.js
@@ -1,6 +1,15 @@
 import React, { useState } from 'react'
-import { Popconfirm, Table, Button, Form, Input } from 'antd'
+import { Popconfirm, Table, Button, Form, Input, Select } from 'antd'
 import { v4 as uuid } from 'uuid'
+import { useEffect } from 'react'
+import axios from 'axios'
+
+
+const SUBJECT_LIST = [
+  {id: 1, name: 'reactjs'},
+  {id: 2, name: 'nodejs'},
+  {id: 3, name: 'veujs'},
+]
 
 const initData = [
   {
@@ -8,21 +17,56 @@ const initData = [
     name: 'Edward King 0',
     age: '32',
     address: 'London, Park Lane no. 0',
+    subject: 1,
   },
   {
     key: '1',
     name: 'Edward King 1',
     age: '32',
     address: 'London, Park Lane no. 1',
+    subject: 2,
   },
 ]
 
-const EditableCell = ({children, editing, dataIndex}) => {
+const EditableCell = ({children, editing, dataIndex, inputType, selectList, currentSelected}) => {
+  const isNomalMode = ['text', 'number', 'textarea'].includes(inputType)
+  const isSelectMode = inputType === 'select'
+  let Component = Input
+  if (inputType === 'textarea') {
+    Component = Input.TextArea
+  }
+
+  let render = null
+  if (isNomalMode) {
+    render = <Component type={inputType} />
+  } else if(isSelectMode) {
+    render = (
+      <Select InitialValues={currentSelected}>
+      {
+        selectList.map(item => {
+          return <Select.Option key={item.id} value={item.id}>{item.name}</Select.Option>
+        })
+      }
+    </Select>  
+    )
+  }
+
+
   return <td>
     {
       editing ? (
         <Form.Item name={dataIndex}>
-          <Input />
+          {/* {isNomalMode && <Component type={inputType} />}
+          {isSelectMode && (
+            <Select defaultValue={currentSelected}>
+              {
+                selectList.map(item => {
+                  return <Select.Option value={item.id}>{item.name}</Select.Option>
+                })
+              }
+            </Select>
+          )} */}
+          {render}
         </Form.Item>
       ) : (
         children
@@ -35,6 +79,7 @@ const MyTable = () => {
   const [form] = Form.useForm()
   const [items, setItems] = useState(initData)
   const [editingKey, setEditingKey] = useState('')
+  const [subjexts, setSubjects] = useState([])
 
   const handleDelete = key => {
     const newItems = items.filter(item => item.key !== key)
@@ -44,7 +89,36 @@ const MyTable = () => {
   const isEditing = (record) => record.key === editingKey;
 
   const edit = record => {
+    form.setFieldsValue({
+      name: '',
+      age: '',
+      address: '',
+      ...record,
+    })
 
+    setEditingKey(record.key)
+  }
+
+  const save = (key) => {
+    const record = items.find(item => item.key === key)
+    if (!record) {
+      return;
+    }
+
+    //validate
+
+    //getFormData
+    const value = form.getFieldValue()
+    // console.log(value)
+
+    //save data
+    const index = items.findIndex(item => item.key === key)
+    const item = items[index]
+
+    const newData = { ...item, ...value }
+    items[index] = newData
+    setItems([...items])
+    setEditingKey('')
   }
 
   const columns = [
@@ -53,14 +127,44 @@ const MyTable = () => {
       dataIndex: 'name',
       width: '30%',
       editable: true,
+      onCell: (record) => ({
+        record,
+        inputType: 'text',
+        dataIndex: 'name',
+        editing: isEditing(record)
+      })
     },
     {
       title: 'age',
       dataIndex: 'age',
+      onCell: (record) => ({
+        record,
+        inputType: 'number',
+        dataIndex: 'age',
+        editing: isEditing(record)
+      })
     },
     {
       title: 'address',
       dataIndex: 'address',
+      onCell: (record) => ({
+        record,
+        inputType: 'textarea',
+        dataIndex: 'address',
+        editing: isEditing(record)
+      })
+    },
+    {
+      title: 'Subject',
+      dataIndex: 'subject',
+      onCell: (record) => ({
+        record,
+        inputType: 'select',
+        dataIndex: 'subject',
+        selectList: subjexts,
+        currentSelected: record.subject,
+        editing: isEditing(record)
+      })
     },
     {
       title: 'operation',
@@ -73,10 +177,19 @@ const MyTable = () => {
         const editable = isEditing(record)
 
         return (<>
-          <Button onClick={() => edit(record)}>Edit</Button>
+          {
+            editable ? (
+              <>
+                <Button onClick={() => save(record.key)}>Save</Button>
+                <Button onClick={() => setEditingKey('')}>Cancel</Button>
+              </>
+            ) : (
+              <Button onClick={() => edit(record)}>Edit</Button>
+            )
+          }
 
           <Popconfirm title="Sure to delete?" onConfirm={() => handleDelete(record.key)}>
-            <a>Delete</a>
+            <a href='#href'>Delete</a>
           </Popconfirm>
         </>)
       }
@@ -91,9 +204,21 @@ const MyTable = () => {
         name: '',
         age: '',
         address: '',
+        
       }
     ])
   }
+
+  const fetchSubjects = () => {
+    axios.get('https://jsonplaceholder.typicode.com/todos')
+    .then(() => {
+      setSubjects(SUBJECT_LIST)
+    })
+  }
+
+  useEffect(() => {
+    fetchSubjects()
+  }, [])
 
   return <>
     <Button onClick={addNewItem}>Add new</Button>
